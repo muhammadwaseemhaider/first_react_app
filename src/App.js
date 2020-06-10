79b3f5a2c46761b4e7182d9a79f1b3fcd231d161
muhammadwaseemhaider <mwaseemhaider74@gmail.com>
2020-06-10 23:00:54 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function Hi() {
+  return <div className="Hi"> Hello <strong>Waseem</strong>
+  <h1 className="Hi_header">This is my first heading</h1>   
+  <ol className="Hi_list">
+    <li>Hello name</li>
+    <li>ordered list</li>
+    <li>unordered list
+    <ul className="Hi_list">
+    <li>Use className</li>
+    <li>Create index.css</li>
+    <li>Add "import './index.css'"</li>
+  </ul>
+    </li>
+  </ol>
+   {5+10}
+
+  </div>;
 }
 
-export default App;
+export default Hi;
